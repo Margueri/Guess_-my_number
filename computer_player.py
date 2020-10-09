ef30ab6e735cc7ed8f66efd1c75cbf1dd2c93f8c
Marguerite Larriere <marguerite.larriere@epfl.ch>
2020-10-09 16:29:59 +0200

    Amélioration 1 de computer_player.py avec ajustement de min et max.

--- computer_player.py
+++ computer_player.py
@@ -5,3 +5,21 @@ Created on Fri Oct  9 17:05:29 2020
 @author: Marguerite Larriere
 """
 
+import random
+
+from guess_my_number import MIN, MAX, GuessMachine
+
+if __name__=='__main__':
+    guess_machine=GuessMachine()
+    while True:
+        attempt=random.randint(MIN,MAX)
+        result=guess_machine.guess(attempt)
+        print('tried %d: %s' % (attempt,result))
+        if result=='found':
+            print('Finished in %d attempts.' % guess_machine.number_of_attempt)
+            break
+        elif result=='too low':
+            min=attempt+1
+        else:
+            max=attempt-1
+            
